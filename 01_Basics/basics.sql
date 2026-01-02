7090c6858d910af98c6e34671858a24b768aee45
Gorli-Jahanavi <jahanavigorli3@gmail.com>
2026-01-02 21:32:48 +0530

    Add MySQL basics

---
+++ 01_Basics/basics.sql
@@ -0,0 +1,28 @@
+-- ============================
+-- Topic: MySQL Basics
+-- ============================
+
+-- A database is like a folder
+-- A table is like an Excel sheet
+-- Columns = headings
+-- Rows = data
+
+-- Create a database
+--syntax : CREATE DATABASE database_name;
+CREATE DATABASE college;
+
+-- Show all databases
+SHOW DATABASES;
+
+-- Select the database to work with
+USE college;
+
+-- Show current database
+SELECT DATABASE();
+
+--and to show all databases use commanad called
+SHOW DATABASES;
+
+
+
+
